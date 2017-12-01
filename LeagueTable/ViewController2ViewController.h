5da8844b5352a4c6324228cc55af332234c0d997
Syed Rubbani [el16smr] <el16smr@eentpc95.leeds.ac.uk>
2017-12-01 13:01:50 +0000

    Added second view controller

---
+++ LeagueTable/ViewController2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2ViewController.h
+//  LeagueTable
+//
+//  Created by Syed Rubbani [el16smr] on 01/12/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2ViewController : UIViewController
+
+@end
